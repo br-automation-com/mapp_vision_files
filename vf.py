f09c294f6f0c0e0643dad24eb3f7fcaf1bdfb1e9
tomas.vostrel <tomas.vostrel@br-automation.com>
2020-10-26 12:08:43 +0100

    Outputs check boxes added. Layout will be changed.

--- vf.py
+++ vf.py
@@ -9,6 +9,11 @@ def on_currentIndexChanged(idx):
                 chk_in[i].setVisible(True)
             else:
                 chk_in[i].setVisible(False)
+        for i in range(len(chk_out)):
+            if mp_out_cr[i]:
+                chk_out[i].setVisible(True)
+            else:
+                chk_out[i].setVisible(False)        
     if idx == 1:
         # Blob
         for i in range(len(chk_in)):
@@ -16,6 +21,11 @@ def on_currentIndexChanged(idx):
                 chk_in[i].setVisible(True)
             else:
                 chk_in[i].setVisible(False)
+        for i in range(len(chk_out)):
+            if mp_out_blob[i]:
+                chk_out[i].setVisible(True)
+            else:
+                chk_out[i].setVisible(False)  
     if idx == 2:
         # Matching
         for i in range(len(chk_in)):
@@ -23,29 +33,54 @@ def on_currentIndexChanged(idx):
                 chk_in[i].setVisible(True)
             else:
                 chk_in[i].setVisible(False)
+        for i in range(len(chk_out)):
+            if mp_out_match[i]:
+                chk_out[i].setVisible(True)
+            else:
+                chk_out[i].setVisible(False)  
     if idx == 3:
-        # Matching
+        # OCR
         for i in range(len(chk_in)):
             if mp_in_ocr[i]:
                 chk_in[i].setVisible(True)
             else:
                 chk_in[i].setVisible(False)
+        for i in range(len(chk_out)):
+            if mp_out_ocr[i]:
+                chk_out[i].setVisible(True)
+            else:
+                chk_out[i].setVisible(False)  
     if idx == 4:
-        # Matching
+        # Measurement
         for i in range(len(chk_in)):
             if mp_in_meas[i]:
                 chk_in[i].setVisible(True)
             else:
                 chk_in[i].setVisible(False)
+        for i in range(len(chk_out)):
+            if mp_out_meas[i]:
+                chk_out[i].setVisible(True)
+            else:
+                chk_out[i].setVisible(False)  
 
 
-def chk_stateChanged():
+def chk_in_stateChanged():
     if chk_in_SelectAll.isChecked():
         for i in range(len(chk_in)):
             chk_in[i].setChecked(True)
     else:
         for i in range(len(chk_in)):
             chk_in[i].setChecked(False)
+
+
+def chk_out_stateChanged():
+    if chk_out_SelectAll.isChecked():
+        for i in range(len(chk_out)):
+            chk_out[i].setChecked(True)
+    else:
+        for i in range(len(chk_out)):
+            chk_out[i].setChecked(False)
+
 
 if __name__ == "__main__":
     app = QApplication([])
@@ -56,10 +91,10 @@ if __name__ == "__main__":
     cbx_vf.addItem('Matching')
     cbx_vf.addItem('OCR')
     cbx_vf.addItem('Measurement')
-
     cbx_vf.currentIndexChanged.connect(on_currentIndexChanged)
 
     chk_in_SelectAll = QCheckBox('Select all')
+    chk_in_SelectAll.stateChanged.connect(chk_in_stateChanged)
 
     chk_in = [  QCheckBox('Enable'),
                 QCheckBox('NumSearchMax'),
@@ -87,26 +122,72 @@ if __name__ == "__main__":
     mp_in_ocr = [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1]
     mp_in_meas = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
 
+    layout = QGridLayout() 
+    layout.addWidget(QLabel('Select your Vision function: '), 0, 0) 
+    layout.addWidget(cbx_vf, 0, 1) 
+ 
+    layout.addWidget(QLabel('_____________________________________________'), 1, 0) 
+    layout.addWidget(QLabel('Inputs'), 2, 0) 
+    layout.addWidget(chk_in_SelectAll, 3, 0) 
+    layout.addWidget(QLabel('_____________________________________________'), 4, 0) 
+     
+    POS_CHK_IN_1 = 5 
+    POS_CHK_IN_2 = 0 
+    for i in range(len(chk_in)): 
+        layout.addWidget(chk_in[i], POS_CHK_IN_1 + i, POS_CHK_IN_2) 
+
     # Set initial inputs visibility
     for i in range(len(chk_in)):
         if mp_in_cr[i] == 0:
             chk_in[i].setVisible(False)
 
-    chk_in_SelectAll.stateChanged.connect(chk_stateChanged)
-    
-    layout = QGridLayout()
-    layout.addWidget(QLabel('Select your Vision function: '), 0, 0)
-    layout.addWidget(cbx_vf, 0, 1)
+    chk_out_SelectAll = QCheckBox('Select all')
+    chk_out_SelectAll.stateChanged.connect(chk_out_stateChanged)
 
-    layout.addWidget(QLabel('_____________________________________________'), 1, 0)
-    layout.addWidget(QLabel('Inputs'), 2, 0)
-    layout.addWidget(chk_in_SelectAll, 3, 0)
-    layout.addWidget(QLabel('_____________________________________________'), 4, 0)
+    chk_out = [ QCheckBox('NumResults'),
+                QCheckBox('DecodedData'),
+                QCheckBox('SymbolType'),
+                QCheckBox('OCRData'),
+                QCheckBox('GradingValue'),
+                QCheckBox('Clipped'),
+                QCheckBox('Area'),
+                QCheckBox('Score'),
+                QCheckBox('PositionX'),
+                QCheckBox('PositionY'),
+                QCheckBox('Orientation'),
+                QCheckBox('Result'),
+                QCheckBox('FunctionProccessingTime'),
+                QCheckBox('EnhancedGradingInformation'),
+                QCheckBox('MeanGrayValue'),
+                QCheckBox('Length'),
+                QCheckBox('Width'),
+                QCheckBox('ModelNumber'),
+                QCheckBox('Scale'),
+                QCheckBox('RotCenterX'),
+                QCheckBox('RotCenterY'),
+                QCheckBox('Xmin'),
+                QCheckBox('Xmax'),
+                QCheckBox('Ymin'),
+                QCheckBox('Ymax'),
+                QCheckBox('Circularity'),
+                QCheckBox('Rectangularity'),
+                QCheckBox('Anisometry')
+            ]
+    mp_out_cr = [1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+    mp_out_blob = [1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    mp_out_match = [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+    mp_out_ocr = [1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    mp_out_meas = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    layout.addWidget(QLabel('_____________________________________________'), 1, 1)
+    layout.addWidget(QLabel('Outputs'), 2, 1)
+    layout.addWidget(chk_out_SelectAll, 3, 1)
+    layout.addWidget(QLabel('_____________________________________________'), 4, 1)
     
-    POS_CHK_IN_1 = 5
-    POS_CHK_IN_2 = 0
-    for i in range(len(chk_in)):
-        layout.addWidget(chk_in[i], POS_CHK_IN_1 + i, POS_CHK_IN_2)
+    POS_CHK_OUT_1 = 5
+    POS_CHK_OUT_2 = 1
+    for i in range(len(chk_out)):
+        layout.addWidget(chk_out[i], POS_CHK_OUT_1 + i, POS_CHK_OUT_2)
 
     widget = QWidget()
     widget.setLayout(layout)
